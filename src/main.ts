422ee33f92dcc7e3d056691bb5a2f99ed8fffe89
tshadz <thais.sombra@hotmail.com>
2024-08-07 08:53:13 -0300

    fix: fix cors vulnerabilities

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,46 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PrismaService } from './external/driven/infra/database/prisma.service';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prismaService = app.get(PrismaService);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://your-allowed-origin.com'], // Substitua pelo domínio que você deseja permitir
+    credentials: true, // Habilitar o suporte a credenciais
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
+    allowedHeaders: 'Content-Type, Accept', // Cabeçalhos permitidos
+  });
+  // Configurar helmet para adicionar cabeçalhos de segurança
+  app.use(helmet());
+
+  // Configurar CSP usando helmet
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", 'https://trusted-cdn.com'],
+        styleSrc: ["'self'", 'https://trusted-cdn.com'],
+        imgSrc: ["'self'", 'https://images.example.com'],
+        connectSrc: ["'self'", 'https://api.example.com'],
+        fontSrc: ["'self'", 'https://fonts.example.com'],
+        objectSrc: ["'none'"],
+        frameSrc: ["'none'"],
+        upgradeInsecureRequests: [], // Para forçar a HTTPS
+      },
+    }),
+  );
+
+  // Adicionar o cabeçalho Permissions-Policy manualmente
+  app.use((req, res, next) => {
+    res.setHeader(
+      'Permissions-Policy',
+      'fullscreen=(self), geolocation=(), microphone=(), camera=()',
+    );
+    next();
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Fast Food')
