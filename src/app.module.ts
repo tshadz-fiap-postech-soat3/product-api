f0224a6b428b0ee29d32fe4c0e1d2821379cf34e
tshadz <thais.sombra@hotmail.com>
2024-08-07 08:17:23 -0300

    add healthz endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ProductsModule } from './@core/products/products.module';
 import { DatabaseModule } from './external/driven/infra/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { CategoriesModule } from './@core/category/categories.module';
+import { HealthController } from './health.controller';
 @Module({
   imports: [
     CategoriesModule,
@@ -10,5 +11,6 @@ import { CategoriesModule } from './@core/category/categories.module';
     DatabaseModule,
     ConfigModule.forRoot(),
   ],
+  controllers: [HealthController],
 })
 export class AppModule {}
